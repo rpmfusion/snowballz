9068ad842f435269a1f62d8cdfb39987faeb561f
Orcan Ogetbil <orcanbahri@yahoo.com>
2009-05-12 20:50:04 +0000

    * Tue May 12 2009 Orcan Ogetbil <oget [DOT] fedora [AT] gmail [DOT] com> 1.0-0.7.beta1.20090512hg - New snapshot

--- snowballz-snapshot.sh
+++ snowballz-snapshot.sh
@@ -25,11 +25,17 @@ hg=$(date +%Y%m%d)hg
 name=snowballz
 
 cd "$tmp"
-hg clone http://freehg.org/u/joey/snowballz/
+#hg clone http://freehg.org/u/joey/snowballz/
+hg clone http://bitbucket.org/joey101/snowballz/
 rm -fr $name/.hg*
 # Removing the shipped fonts
 # No fonts are allowed in Fedora in non-font packages
 rm $name/data/*.ttf
+cd $name
+# Get the snowui which will be a part of snowballz final release
+hg clone http://freehg.org/u/joey/snowui/ 
+rm -fr snowui/.hg*
+cd ..
 tar jcf "$pwd"/$name-$version.$hg.tar.bz2 $name
 echo "Written: $name-$version-$hg.tar.bz2"
 cd - >/dev/null
